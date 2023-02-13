13294bd4d2e4bc4918301fd0e4c58c8691f356e9
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2023-02-13 09:21:40 -0300

    update

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { GameSounds as GameSound, Player } from "./core";
+import { GameSound, Player } from "./core";
 
 export function validate<T extends z.ZodSchema>(
   data: unknown,
