307dc138bbf6ee0072d64410d0a156771b67a49a
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2023-12-20 15:41:51 -0300

    feat: change players UI

--- src/steps/PlayGame.tsx
+++ src/steps/PlayGame.tsx
@@ -13,14 +13,17 @@ import {
   PencilIcon,
   TrophyIcon,
 } from "../components/Icons";
-import { getGuessWordBonus, useGameAction, useGameState } from "../core";
+import {
+  getGuessWordBonus,
+  Player,
+  useGameAction,
+  useGameState,
+} from "../core";
 import { cn } from "../utils";
 
 export function PlayGame() {
-  const state = useGameState("playing");
-
   return (
-    <div className="flex px-4 gap-16 flex-col h-screen">
+    <div className="flex gap-16 flex-col h-screen">
       <Players />
 
       <div className="flex flex-col gap-8 items-center flex-1">
@@ -34,105 +37,58 @@ export function PlayGame() {
   );
 }
 
+type PlayerWithState = Player & {
+  state: "host" | "turn" | "waiting" | "winner";
+};
+
 function Players() {
   const { game, round } = useGameState("playing");
-  const players = [...game.players]
+
+  const { host, players } = [...game.players]
     .map((player) => {
       const isHost = round.host === player.id;
-      const isTurn = round.turn === player.id;
+      const isTurn = round.turn === player.id && round.winner == null;
+      const isWinner = round.winner === player.id;
 
       return {
         ...player,
-        state: isHost ? "host" : isTurn ? "turn" : "waiting",
-      };
+        state: isHost
+          ? "host"
+          : isTurn
+          ? "turn"
+          : isWinner
+          ? "winner"
+          : "waiting",
+      } satisfies PlayerWithState;
     })
-    .sort((a, b) => (a.state === "host" ? -1 : b.state === "host" ? 1 : 0));
+    .reduce(
+      (state, player) => {
+        if (player.state === "host") {
+          state.host = player;
+        } else {
+          state.players.push(player);
+        }
+
+        return state;
+      },
+      {
+        host: {} as any,
+        players: [],
+      } as {
+        host: PlayerWithState;
+        players: PlayerWithState[];
+      }
+    );
 
   return (
-    <header className="p-3 pt-12 gap-3 flex w-full justify-center bg--neutral-900">
-      {players.map((player) => {
-        const isHost = round.host === player.id;
-        const isWinner = round.winner === player.id;
-        const isTurn = round.turn === player.id && round.winner == null;
-        const letterGuesses = round.playerGuesses[player.id].letters;
-        const wordGuesses = round.playerGuesses[player.id].words;
-        const isOut = wordGuesses > 0 ? false : letterGuesses === 0;
-        const roundPoints = round.points[player.id];
+    <header className="p-3 flex w-full justify-between bg-neutral-900">
+      <PlayerCard player={host} />
 
-        return (
-          <div
-            key={player.id}
-            className={cn(
-              "flex min-w-[200px] flex-col text-white rounded-lg overflow-hidden relative",
-              isWinner
-                ? "bg-green-700 text-black/70 animate-bounce"
-                : isHost && round.winner && !isWinner
-                ? "bg-red-900"
-                : isHost
-                ? "bg-info-700"
-                : isTurn
-                ? "animate--pulse bg-green-600"
-                : isOut
-                ? "bg-neutral-900/20 text-black/20"
-                : "bg-neutral-700"
-            )}
-          >
-            <div className="flex justify-between p-2 px-4">
-              <div
-                className={cn(
-                  "flex items-center justify-center gap-2 text-3xl uppercase font-bold flex-grow-0 tracking-wider"
-                )}
-              >
-                <span>{player.name}</span>
-              </div>
-
-              <div className="flex items-center">
-                {isHost && round.winner == null && <PencilIcon />}
-                {isTurn && (
-                  <div className="animate-bounce">
-                    <ChatIcon />
-                  </div>
-                )}
-                {isWinner && <TrophyIcon />}
-                {isOut && <BlockIcon />}
-              </div>
-
-              <div className="text-3xl font-extrabold">
-                {game.playerPoints[player.id]}
-              </div>
-            </div>
-
-            <div
-              className={cn(
-                "flex-1 flex justify-between font-bold p-2 bg-neutral-800/30 uppercase",
-                isOut && "bg-transparent"
-              )}
-            >
-              {isHost ? null : (
-                <div className="flex justify-around flex-[2]">
-                  <div className="flex-1 text-center">
-                    <div className="text-[10px]">Letras</div>
-                    <div className="text-2xl">{letterGuesses}</div>
-                  </div>
-                  <div className="flex-1 text-center">
-                    <div className="text-[10px]">Chutes</div>
-                    <div className="text-2xl">{wordGuesses}</div>
-                  </div>
-                </div>
-              )}
-
-              <div className="h-full flex items-center justify-end flex-1">
-                <div className="flex items-center justify-center">
-                  <div className="text-xl">
-                    {roundPoints === 0 ? "" : roundPoints > 0 ? "+" : "-"}
-                  </div>
-                  <div className="text-4xl">{Math.abs(roundPoints)}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+      <div className="flex gap-3">
+        {players.map((player) => (
+          <PlayerCard key={player.id} player={player} />
+        ))}
+      </div>
     </header>
   );
 }
@@ -514,5 +470,92 @@ function Actions() {
         Restart
       </button>
     </form>
+  );
+}
+
+function PlayerCard({ player }: { player: PlayerWithState }) {
+  const { round, game } = useGameState("playing");
+
+  const isHost = player.state === "host";
+  const isWinner = player.state === "winner";
+  const isTurn = player.state === "turn";
+
+  const letterGuesses = round.playerGuesses[player.id].letters;
+  const wordGuesses = round.playerGuesses[player.id].words;
+  const isOut = wordGuesses > 0 ? false : letterGuesses === 0;
+  const roundPoints = round.points[player.id];
+
+  return (
+    <div
+      key={player.id}
+      className={cn(
+        "flex min-w-[200px] flex-col text-white rounded-lg overflow-hidden relative",
+        isWinner
+          ? "bg-green-700 text-black/70 animate-bounce"
+          : isHost && round.winner && !isWinner
+          ? "bg-red-900"
+          : isHost
+          ? "bg-info-700"
+          : isTurn
+          ? "animate--pulse bg-green-600"
+          : isOut
+          ? "opacity-50"
+          : "bg-neutral-800"
+      )}
+    >
+      <div className="flex justify-between p-2 px-4">
+        <div
+          className={cn(
+            "flex items-center justify-center gap-2 text-3xl uppercase font-bold flex-grow-0 tracking-wider"
+          )}
+        >
+          <span>{player.name}</span>
+        </div>
+
+        <div className="flex items-center">
+          {isHost && round.winner == null && <PencilIcon />}
+          {isTurn && (
+            <div className="animate-bounce">
+              <ChatIcon />
+            </div>
+          )}
+          {isWinner && <TrophyIcon />}
+          {isOut && <BlockIcon />}
+        </div>
+
+        <div className="text-3xl font-extrabold">
+          {game.playerPoints[player.id]}
+        </div>
+      </div>
+
+      <div
+        className={cn(
+          "flex-1 flex justify-between font-bold p-2 bg-neutral-800/30 uppercase",
+          isOut && "bg-transparent"
+        )}
+      >
+        {isHost ? null : (
+          <div className="flex justify-around flex-[2]">
+            <div className="flex-1 text-center">
+              <div className="text-[10px]">Letras</div>
+              <div className="text-2xl">{letterGuesses}</div>
+            </div>
+            <div className="flex-1 text-center">
+              <div className="text-[10px]">Chutes</div>
+              <div className="text-2xl">{wordGuesses}</div>
+            </div>
+          </div>
+        )}
+
+        <div className="h-full flex items-center justify-end flex-1">
+          <div className="flex items-center justify-center">
+            <div className="text-xl">
+              {roundPoints === 0 ? "" : roundPoints > 0 ? "+" : "-"}
+            </div>
+            <div className="text-4xl">{Math.abs(roundPoints)}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
