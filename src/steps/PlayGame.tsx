96e354b6375c2891b2c92cd91f3ccb4d7e4a4f09
Igor Ribeiro <igor-ribeiro@users.noreply.github.com>
2025-01-02 16:14:43 -0300

    feat: add timer

--- src/steps/PlayGame.tsx
+++ src/steps/PlayGame.tsx
@@ -3,8 +3,9 @@ import { WrongGuesses } from "../components/WrongGuesses";
 import { GuessOptions } from "../components/GuessOptions";
 import { Players } from "../components/Players";
 import { WordPanel } from "../components/WordPanel";
-import { useGameState } from "../core";
+import { useGameAction, useGameState } from "../core";
 import { cn } from "../utils";
+import { useEffect, useRef, useState } from "react";
 
 export function PlayGame() {
   const { round } = useGameState("playing");
@@ -17,6 +18,8 @@ export function PlayGame() {
         <WordPanel />
 
         <GuessOptions />
+
+        {round.timer > 0 && <Timer />}
       </div>
 
       <div
@@ -28,6 +31,90 @@ export function PlayGame() {
         <WrongGuesses />
         <Actions />
       </div>
+    </div>
+  );
+}
+
+function useTimer(initial: number) {
+  const timeoutRef = useRef<number>(-1);
+  const [time, setTime] = useState(initial);
+
+  function start() {
+    clear();
+
+    timeoutRef.current = setInterval(() => {
+      setTime((t) => Math.max(-1, t - 1));
+    }, 1000);
+  }
+  function reset(time?: number) {
+    clear();
+    setTime(time ?? initial);
+  }
+
+  function clear() {
+    if (timeoutRef.current) {
+      clearInterval(timeoutRef.current);
+    }
+  }
+
+  function stop() {
+    clear();
+  }
+
+  function restart() {
+    reset();
+    start();
+  }
+
+  return {
+    start,
+    reset,
+    restart,
+    stop,
+    time,
+  };
+}
+
+function Timer() {
+  const { round } = useGameState("playing");
+  const action = useGameAction();
+  const { time, start, restart, reset } = useTimer(round.timer);
+
+  useEffect(() => {
+    start();
+
+    return reset;
+  }, []);
+
+  useEffect(() => {
+    if (round.winner) {
+      reset();
+      return;
+    }
+
+    reset(round.timer);
+    start();
+  }, [round.timer, round.id, round.guessMode, round.winner]);
+
+  useEffect(() => {
+    if (time < 0) {
+      action({ type: "skip" });
+      restart();
+    }
+  }, [time]);
+
+  return (
+    <div
+      className={cn(
+        "p-4 font-black text-4xl rounded-md border-inside w-20 text-center text-black/70",
+        time < 11
+          ? "bg-red-400 animate-bounce"
+          : time < 20
+          ? "bg-yellow-500"
+          : "bg-white"
+      )}
+    >
+      {time}
     </div>
   );
 }
